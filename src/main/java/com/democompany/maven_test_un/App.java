655032219b41b926ff83004a65f6b8b2a32a1d14
PaulinPrize <prisoprisopaulinarnold@gmail.com>
2023-07-12 11:17:05 -0400

    Maven projet test un du 12_07_23

---
+++ src/main/java/com/democompany/maven_test_un/App.java
@@ -0,0 +1,13 @@
+package com.democompany.maven_test_un;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
